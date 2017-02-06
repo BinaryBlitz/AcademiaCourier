579e7047cc3447c0e50137379a61db4390805f16
Efanov Evgeny <efanovev1995@gmail.com>
2017-02-06 18:19:57 +0300

    Code style fix for AuthActivity (#2)
    * Code style fix for authActivity
    
    * Remove space from string resourse

--- app/src/main/java/com/academiaexpresssystem/Activities/AuthActivity.java
+++ app/src/main/java/com/academiaexpresssystem/Activities/AuthActivity.java
@@ -1,53 +1,25 @@
 package com.academiaexpresssystem.Activities;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import com.academiaexpresssystem.ProgressDialog;
-import com.academiaexpresssystem.Push.QuickstartPreferences;
-import com.academiaexpresssystem.R;
 import com.academiaexpresssystem.Push.RegistrationIntentService;
-import com.academiaexpresssystem.Server.DeliveryServerRequest;
+import com.academiaexpresssystem.R;
 import com.academiaexpresssystem.Server.DeviceInfoStore;
-import com.academiaexpresssystem.Server.OnRequestPerformedListener;
+import com.academiaexpresssystem.Server.ServerApi;
+import com.academiaexpresssystem.Utils.AndroidUtilities;
+import com.google.gson.JsonObject;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AuthActivity extends AppCompatActivity {
-
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    private static final String TAG = "MainActivity";
-
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
-    }
-
-
-    @Override
-    protected void onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
-        super.onPause();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,94 +28,75 @@ public class AuthActivity extends AppCompatActivity {
 
         DeviceInfoStore.resetToken(this);
 
-        final ProgressDialog dialog1 = new ProgressDialog();
-
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                SharedPreferences sharedPreferences =
-                        PreferenceManager.getDefaultSharedPreferences(context);
-                boolean sentToken = sharedPreferences
-                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
-                dialog1.dismiss();
-                Intent intent2 = new Intent(AuthActivity.this, OrdersActivity.class);
-                startActivity(intent2);
-                finish();
-            }
-        };
-
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (check()) {
-                    final ProgressDialog dialog = new ProgressDialog();
-                    dialog.show(getFragmentManager(), "deliverysystemapp");
-
-                    JSONObject object = new JSONObject();
-
-                    try {
-                        object.accumulate("phone_number", ((EditText) findViewById(R.id.edittext1)).getText().toString());
-                        object.accumulate("password", ((EditText) findViewById(R.id.edittext2)).getText().toString());
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    DeliveryServerRequest.with(AuthActivity.this)
-                            .skipAuth()
-                            .objects(object)
-                            .listener(new OnRequestPerformedListener() {
-                                @Override
-                                public void onRequestPerformedListener(Object... objects) {
-                                    dialog.dismiss();
-                                    if (objects[0].equals("Error")) {
-                                        Snackbar.make(findViewById(R.id.main), "Ошибка авторизации.", Snackbar.LENGTH_SHORT).show();
-                                    } else {
-                                        try {
-                                            DeviceInfoStore.saveToken(((JSONObject) objects[0]).getString("api_token"));
-                                           // DeviceInfoStore.saveId(((JSONObject) objects[0]).getString("id"));
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                        if (checkPlayServices()) {
-                                            Intent intent = new Intent(AuthActivity.this, RegistrationIntentService.class);
-                                            startService(intent);
-                                            dialog1.show(getFragmentManager(), "getthelpapp");
-                                        }
-                                    }
-                                }
-                            })
-                            .auth()
-                            .perform();
+                    executeRequest();
                 }
             }
         });
     }
 
-    private boolean check() {
-        if(((EditText) findViewById(R.id.edittext1)).getText().toString().isEmpty()) {
-            Snackbar.make(findViewById(R.id.main), "Поле имени не заполнено.", Snackbar.LENGTH_SHORT).show();
-            return false;
-        } else if(((EditText) findViewById(R.id.edittext2)).getText().toString().isEmpty()) {
-            Snackbar.make(findViewById(R.id.main), "Поле  пароля не заполнено.", Snackbar.LENGTH_SHORT).show();
-            return false;
-        }
+    private JsonObject generateJson() {
+        JsonObject object = new JsonObject();
 
-        return true;
+        object.addProperty("phone_number", ((EditText) findViewById(R.id.edittext1)).getText().toString());
+        object.addProperty("password", ((EditText) findViewById(R.id.edittext2)).getText().toString());
+
+        return object;
     }
 
-    private boolean checkPlayServices() {
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (apiAvailability.isUserResolvableError(resultCode)) {
-                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
-                        .show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-                finish();
+    private void parseAnswer(JsonObject object) {
+        try {
+            DeviceInfoStore.saveToken(this, object.get("api_token").getAsString());
+            DeviceInfoStore.saveId(this, object.get("id").getAsString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (AndroidUtilities.INSTANCE.checkPlayServices(this)) {
+            Intent intent = new Intent(AuthActivity.this, RegistrationIntentService.class);
+            startService(intent);
+        }
+
+        Intent intent = new Intent(AuthActivity.this, OrdersActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void executeRequest() {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.show();
+
+        ServerApi.get(this).api().auth(generateJson()).enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                dialog.dismiss();
+                if (response.isSuccessful()) {
+                    parseAnswer(response.body());
+                } else {
+                    Snackbar.make(findViewById(R.id.main), R.string.auth_error, Snackbar.LENGTH_SHORT).show();
+                }
             }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                dialog.dismiss();
+                Snackbar.make(findViewById(R.id.main), R.string.internet_connection_error, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean check() {
+        if (((EditText) findViewById(R.id.edittext1)).getText().toString().isEmpty()) {
+            Snackbar.make(findViewById(R.id.main), R.string.name_error, Snackbar.LENGTH_SHORT).show();
+            return false;
+        } else if (((EditText) findViewById(R.id.edittext2)).getText().toString().isEmpty()) {
+            Snackbar.make(findViewById(R.id.main), R.string.password_error, Snackbar.LENGTH_SHORT).show();
             return false;
         }
+
         return true;
     }
 }
