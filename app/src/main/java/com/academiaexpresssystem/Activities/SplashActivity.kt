aacc2fbacb0fac40d04bb20fad7e450236c9129b
Efanov Evgeny <efanovev1995@gmail.com>
2017-02-06 20:04:12 +0300

    Code style fix for SplashActivity (#4)
    * Code style fix for SplashActivity
    
    * Fix if statement
    
    * Move activity to open to standalone value
    
    * Add empty line to end of the file

---
+++ app/src/main/java/com/academiaexpresssystem/Activities/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.academiaexpresssystem.Activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.academiaexpresssystem.R
+import com.academiaexpresssystem.Server.DeviceInfoStore
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+        setContentView(R.layout.splash_layout)
+
+        val activity = if (DeviceInfoStore.getToken(this) == "null") AuthActivity::class.java else OrdersActivity::class.java
+        openActivity(activity)
+    }
+
+    private fun openActivity(activity: Class<*>) {
+        val intent = Intent(this@SplashActivity, activity)
+        startActivity(intent)
+        finish()
+    }
+}
