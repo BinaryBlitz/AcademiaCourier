949835f10a5ae0dee0c91f903f307af8a4279332
outboard <efanovev1995@gmail.com>
2017-02-06 18:41:32 +0300

    Code style fix for SplashActivity

---
+++ app/src/main/java/com/academiaexpresssystem/Activities/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.academiaexpresssystem.Activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.academiaexpresssystem.R
+import com.academiaexpresssystem.Server.DeviceInfoStore
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+        setContentView(R.layout.splash_layout)
+
+        if (DeviceInfoStore.getToken(this) == "null") {
+            openActivity(AuthActivity::class.java)
+        } else {
+            openActivity(OrdersActivity::class.java)
+        }
+    }
+
+    private fun openActivity(activity: Class<*>) {
+        val intent = Intent(this@SplashActivity, activity)
+        startActivity(intent)
+        finish()
+    }
+}
